543b38b64c04d16603e2368c1cf31b1264f311bc
980327 <alap_u@naver.com>
2022-03-11 00:14:18 +0900

    백준 1110

---
+++ src/Main.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int a = Integer.parseInt(st.nextToken());
+
+        int e = a;
+        int count = 1;
+        while (true) {
+
+            int b = e / 10;
+            int c = e % 10;
+            int d = b + c;
+
+            e = Integer.parseInt(c + "" + (d % 10) + "");
+            if (e == a) {
+                break;
+            }
+            count++;
+        }
+        System.out.println(count);
+    }
+}
