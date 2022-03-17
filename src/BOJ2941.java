63e903a085f01facb9f9b23fe49e6ce3a263902e
980327 <alap_u@naver.com>
2022-03-17 23:59:36 +0900

    2941

--- src/BOJ2941.java
+++ src/BOJ2941.java
@@ -1,7 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-
+//백준 2941
 class Main {
     public static void main(String[] args) throws IOException {
         String[] strArr = {"c=", "c-", "dz=", "d-", "lj", "nj", "s=", "z="};
