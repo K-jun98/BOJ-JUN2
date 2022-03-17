284eaa04008f300e4dcf1acb111ea86c0fb58d87
980327 <alap_u@naver.com>
2022-03-18 00:12:52 +0900

    2941

--- src/BOJ2941.java
+++ src/BOJ2941.java
@@ -1,6 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+
 //백준 2941
 class Main {
     public static void main(String[] args) throws IOException {
