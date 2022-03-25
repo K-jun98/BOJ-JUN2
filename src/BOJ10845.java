b8d40ea9e4a1a09ccb282bb666275066aa152e8c
980327 <alap_u@naver.com>
2022-03-26 01:12:24 +0900

    10845

--- src/BOJ10845.java
+++ src/BOJ10845.java
@@ -1,16 +1,16 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Stack;
-import java.util.StringTokenizer;
+import java.util.*;
 
- class Main {
+class Main {
 
     public static void main(String args[]) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
-        Stack stack = new Stack();
+        Queue queue = new ArrayDeque();
         StringBuffer sb=new StringBuffer();
+        int num=0;
         for (int i = 0; i < N; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
 
@@ -18,19 +18,23 @@ import java.util.StringTokenizer;
 
 
             if ("push".equals(order)) {
-                stack.push(st.nextToken());
+                num=Integer.parseInt(st.nextToken());
+                queue.add(num);
             }
             if ("pop".equals(order)) {
-                sb.append(stack.size() == 0 ? "-1" : stack.pop()).append("\n");
+                sb.append(queue.size() == 0 ? "-1" : queue.remove()).append("\n");
             }
             if ("size".equals(order)) {
-                sb.append(stack.size()).append("\n");
+                sb.append(queue.size()).append("\n");
             }
             if ("empty".equals(order)) {
-                sb.append(stack.size() == 0 ? "1" : "0").append("\n");
+                sb.append(queue.size() == 0 ? "1" : "0").append("\n");
             }
-            if ("top".equals(order)) {
-                sb.append(stack.size() == 0 ? "-1" : stack.peek()).append("\n");
+            if ("front".equals(order)) {
+                sb.append(queue.size() == 0 ? "-1" : queue.peek()).append("\n");
+            }
+            if ("back".equals(order)) {
+                sb.append(queue.size() == 0 ? "-1" : num).append("\n");
             }
         }
         System.out.println(sb.toString());
