04ee0e30a59e05f9ddef8e680d5421ac4de2e029
980327 <alap_u@naver.com>
2022-03-28 16:12:58 +0900

    2743-단어 길이 재기

--- src/BOJ2743.java
+++ src/BOJ2743.java
@@ -2,20 +2,11 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-//백준 2941
-class Main {
+
+ class Main {
     public static void main(String[] args) throws IOException {
-        String[] strArr = {"c=", "c-", "dz=", "d-", "lj", "nj", "s=", "z="};
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String str = br.readLine();
-
-
-        for (int i = 0; i < strArr.length; i++) {
-            str=str.replace(strArr[i],"*");
-
-        }
+        String str= br.readLine();
         System.out.println(str.length());
-
-
     }
 }
