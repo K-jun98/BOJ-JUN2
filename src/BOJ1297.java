38a7d33cc2e8177bcc9b4313446eda368a265ebf
980327 <alap_u@naver.com>
2022-04-15 16:23:41 +0900

    1297-TV 크기

---
+++ src/BOJ1297.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+ class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int c = sc.nextInt();
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        double result = Math.pow(c, 2) / (Math.pow(a, 2) + Math.pow(b, 2));
+
+        System.out.println((int)Math.sqrt(result * Math.pow(a, 2)) + " " + (int)Math.sqrt(result * Math.pow(b, 2)));
+
+    }
+}
