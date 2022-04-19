25017f767730ac950fd1547140f4da9c36ae2096
980327 <alap_u@naver.com>
2022-04-19 23:03:18 +0900

    2558-A+B - 2

--- src/BOJ2558.java
+++ src/BOJ2558.java
@@ -4,10 +4,10 @@ import java.util.Scanner;
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        while (sc.hasNextLine()) {
-            String input = sc.nextLine();
-            System.out.println(input);
-        }
+        int a = sc.nextInt();
+        int b = sc.nextInt();
         sc.close();
+
+        System.out.println(a + b);
     }
 }
