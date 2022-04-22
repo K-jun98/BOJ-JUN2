e79e57479fbdd5722a458f26d7478fe820dc9c58
980327 <alap_u@naver.com>
2022-04-22 18:23:38 +0900

    2839-설탕 배달

--- src/BOJ2839.java
+++ src/BOJ2839.java
@@ -2,22 +2,22 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-
 class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
+        int cnt = 0;
+        int N = Integer.parseInt(br.readLine());
         while (true) {
-            String orign = br.readLine();
-            if (!orign.equals("END")) {
-                StringBuffer sb = new StringBuffer(orign).reverse();
-                System.out.println(sb);
-            } else
+            if (N % 5 == 0){
+                System.out.println(N/5+cnt);
+            break;
+        }
+            else if (N < 0) {
+                System.out.println("-1");
                 break;
-
+            }
+            N -= 3;
+            cnt++;
         }
     }
 }
-
-
-
