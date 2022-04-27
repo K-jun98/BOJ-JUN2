c37586a88862b11a25ce129a53b01cb5a338a517
980327 <alap_u@naver.com>
2022-04-28 01:43:01 +0900

    1085-직사각형에서 탈출

---
+++ src/BOJ1085.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int x = Integer.parseInt(st.nextToken());
+        int y = Integer.parseInt(st.nextToken());
+        int w = Integer.parseInt(st.nextToken());
+        int h = Integer.parseInt(st.nextToken());
+
+        int minx = Math.min(x, w - x);
+        int miny = Math.min(y, h - y);
+
+        System.out.println(Math.min(minx, miny));
+
+
+    }
+}
+
