849810e005fcb5c7e46a72b3ee043534ab3e1593
980327 <alap_u@naver.com>
2022-05-18 00:48:29 +0900

    10799-쇠막대기

---
+++ src/BOJ10799.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+import java.util.Stack;
+
+class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String input = sc.next();
+        int result = 0;
+        Stack<Integer> stack = new Stack<Integer>();
+
+        for(int i = 0; i < input.length(); i++) {
+            if(input.charAt(i) == '(')
+                stack.push(i);
+            else {
+                if(stack.pop() == i-1)
+                    result+=stack.size();
+                else
+                    result+=1;
+            }
+        }
+
+        System.out.println(result);
+
+        sc.close();
+        return;
+    }
+}
