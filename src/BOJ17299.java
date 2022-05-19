76e22e5c8c880ac3e588c0e1f40bc48a2af29830
980327 <alap_u@naver.com>
2022-05-20 00:52:11 +0900

    17299-오등큰수

--- src/BOJ17299.java
+++ src/BOJ17299.java
@@ -11,26 +11,22 @@ class Main {
         int N = Integer.parseInt(br.readLine());
         StringTokenizer st = new StringTokenizer(br.readLine());
         int[] arr = new int[N];
+        int[] cnt = new int[1_000_001];
         for (int i = 0; i < N; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
-        for (int i = 0; i < N - 1; i++) {
-            if (arr[i] < arr[i + 1]) {
-                arr[i] = arr[i + 1];
-                while (!stack.empty()) {
-                    if (arr[stack.peek()] < arr[i + 1]) {
-                        arr[stack.pop()] = arr[i + 1];
-                    } else {
-                        break;
-                    }
-                }
-            } else
-                stack.push(i);
+        for (int i = 0; i < N; i++) {
+            cnt[arr[i]]++;
+        }
+        for (int i = 0; i < N; i++) {
+            while (!stack.empty() && cnt[arr[stack.peek()]] < cnt[arr[i]]) {
+                arr[stack.pop()] = arr[i];
+            }
+            stack.push(i);
         }
         while (!stack.empty()) {
             arr[stack.pop()] = -1;
         }
-        arr[arr.length - 1] = -1;
         for (int i : arr) {
             sb.append(i).append(" ");
         }
