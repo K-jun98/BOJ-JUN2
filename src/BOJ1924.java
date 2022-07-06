314261593bdb51955eb2765db7f0b570ed687b02
980327 <alap_u@naver.com>
2022-07-06 20:07:36 +0900

    1924 - 2007년

---
+++ src/BOJ1924.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int month = Integer.parseInt(st.nextToken());
+        int day = Integer.parseInt(st.nextToken());
+
+        int[] daysInMonth = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+        String[] dayOfTheWeeks = {"SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"};
+
+        int totalDay = day;
+        for (int i = 0; i < month - 1; ++i) {
+            totalDay += daysInMonth[i];
+        }
+
+        System.out.println(dayOfTheWeeks[totalDay % 7]);
+    }
+}
