b4c6b7d704761551438ba19952c9a98ad8b461db
980327 <alap_u@naver.com>
2022-07-18 13:58:15 +0900

    10991 - 별 찍기 - 16

---
+++ src/BOJ10991.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args)  {
+
+
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+
+        for (int i = 1; i <= N; i++) {
+
+            for (int j = 1; j <= N-i ; j++) {
+                System.out.print(" ");
+            }
+
+            for (int j = 1; j <= i+(i-1); j++) {
+                if(j%2==0)
+                    System.out.print(" ");
+                else
+                    System.out.print("*");
+            }
+            System.out.println();
+        }
+
+    }
+}
