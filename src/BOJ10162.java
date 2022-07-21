535e960790b0c8e61b91d2d20ce6f2e843921f39
980327 <alap_u@naver.com>
2022-07-21 21:17:17 +0900

    10162 - 전자레인지

---
+++ src/BOJ10162.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    static int a;
+    static int b;
+    static int c;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        a = 0;
+        b=0;
+        c=0;
+        if (n % 10 != 0) {
+            System.out.print(-1);
+            return;
+        }
+        f(n);
+        System.out.print(a + " " + b + " " + c);
+
+    }
+
+    static void f(int n) {
+        if (n == 0) return;
+        if (n >= 300) {
+            n-= 300;
+            a++;
+            f(n);
+        } else if (n >= 60) {
+            n-=60;
+            b++;
+            f(n);
+        } else {
+            n-= 10;
+            c++;
+            f(n);
+        }
+    }
+}
