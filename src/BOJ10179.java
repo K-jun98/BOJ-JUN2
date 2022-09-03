7a648d1309ca242556c9ecf6a706a5a822d57730
gobeomjun <alap_u@naver.com>
2022-09-04 08:05:44 +0900

    BOJ10179 - 쿠폰

---
+++ src/BOJ10179.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int n = scan.nextInt();
+        double[] arr = new double[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = scan.nextDouble();
+            double sum = arr[i] * 80 / 100;
+            System.out.println("$" + String.format("%.2f", sum));
+        }
+    }
+}
