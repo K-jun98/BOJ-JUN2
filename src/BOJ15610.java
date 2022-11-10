04b146c19ff7c8805b5999f46eb39db013dda7c6
K-jun98 <alap_u@naver.com>
2022-11-10 21:04:53 +0900

    BOJ15610 - Abbey Courtyard

---
+++ src/BOJ15610.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class BOJ15610 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        long area = scanner.nextLong();
+        System.out.println(Math.sqrt(area)*4);
+    }
+
+}
