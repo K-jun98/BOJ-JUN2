c75fde4286142d0042a14e5e7b33854154a275bc
jun <alap_u@naver.com>
2023-10-01 21:28:21 +0900

    BOJ1004.java

--- src/BOJ1004.java
+++ src/BOJ1004.java
@@ -1,5 +1,7 @@
-import java.util.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
 
