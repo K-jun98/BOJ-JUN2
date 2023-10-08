96e82ecc43d5690490385734a72fd4cadb8f7119
jun <alap_u@naver.com>
2023-10-09 05:35:40 +0900

    BOJ2805.java

---
+++ src/BOJ2805.java
@@ -0,0 +1,53 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Solution {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static int[] arr;
+
+    public static void main(String[] args) throws IOException {
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        arr = new int[N];
+        long big = 0;
+
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            int line = Integer.parseInt(st.nextToken());
+            arr[i] = line;
+            big = Math.max(line, big);
+        }
+
+        long low = 1;
+        long mid=0;
+        long answer = 0;
+        while (low <= big) {
+            mid = (low + big) / 2;
+            long count = count(mid);
+
+            if (count >= M) {
+                low = mid + 1;
+                answer = mid;
+            } else {
+                big = mid - 1;
+            }
+        }
+
+        System.out.println(answer);
+    }
+
+    public static long count(long length) {
+        long answer = 0;
+        for (int i = 0; i < arr.length; i++) {
+            long answer1 = arr[i] - length;
+            if (answer1 > 0) {
+                answer += answer1;
+            }
+        }
+        return answer;
+    }
+}
