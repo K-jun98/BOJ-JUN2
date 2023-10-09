ebd4d3ff477465f14a36f0cbe08e36bc794210f9
jun <alap_u@naver.com>
2023-10-09 23:56:40 +0900

    BOJ1010.java

--- src/BOJ1010.java
+++ src/BOJ1010.java
@@ -1,5 +1,8 @@
-import java.util.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.List;
 import java.util.stream.Collectors;
 
 public class Main {
