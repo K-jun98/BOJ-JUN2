967f77bdf81e9a5485b750a68b7ab153b69742a3
jun <alap_u@naver.com>
2023-10-12 23:27:37 +0900

    BOJ1026.java

--- src/BOJ1026.java
+++ src/BOJ1026.java
@@ -1,6 +1,10 @@
-
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.StringTokenizer;
 
 
 class Main {
