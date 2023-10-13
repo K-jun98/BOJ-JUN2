3806a00109ade19e8ea99a32e703a744738ede2d
jun <alap_u@naver.com>
2023-10-13 23:45:38 +0900

    BOJ1026.java

--- src/BOJ1037.java
+++ src/BOJ1037.java
@@ -1,6 +1,6 @@
 import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -14,9 +14,9 @@ public class Main {
         int max = Integer.MIN_VALUE;
         int min = Integer.MAX_VALUE;
 
-        StringTokenizer st = new StringTokenizer(br.readLine()," ");
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 
-        while(T-- > 0) {
+        while (T-- > 0) {
             int N = Integer.parseInt(st.nextToken());
             max = N > max ? N : max;
             min = N < min ? N : min;
